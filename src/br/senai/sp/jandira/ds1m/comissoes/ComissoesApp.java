80fc0b2f1690852f95934132d093e35b6190b282
CELSO MARCOS FURTADO <sn1006137@CFP127ALUNO.LOCAL>
2025-09-15 11:25:17 -0300

    Projeto para aprender a utilizar o IF

---
+++ src/br/senai/sp/jandira/ds1m/comissoes/ComissoesApp.java
@@ -0,0 +1,15 @@
+package br.senai.sp.jandira.ds1m.comissoes;
+
+import br.senai.sp.jandira.ds1m.comissoes.model.Vendedor;
+
+import java.util.Scanner;
+
+public class ComissoesApp {
+    public static void main(String[] args) {
+        System.out.println("Iniciando o programa...");
+
+        Vendedor vendedor = new Vendedor();
+        vendedor.obterDados();
+
+    }
+}
---
+++ src/br/senai/sp/jandira/ds1m/comissoes/model/Vendedor.java
@@ -0,0 +1,53 @@
+package br.senai.sp.jandira.ds1m.comissoes.model;
+
+import java.util.Scanner;
+
+public class Vendedor {
+    String nomeVendedor;
+    double totalDeVendas;
+    double percentualDaComissao;
+    double valorDaComissaoEmReais;
+
+    public void obterDados(){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("** CALCULADORA DE COMISSÕES **");
+        System.out.println("-----------------------------------");
+
+        // Obtendo o nome do vendedor
+        System.out.print("Qual o nome do vendedor? ");
+        nomeVendedor = leitor.nextLine();
+
+        // Obtendo o total de vendas do vendedor
+        System.out.print("Qual o total de vendas do(a) vendedor(a) " + nomeVendedor + "? ");
+        totalDeVendas = leitor.nextDouble();
+
+        calcularComissao();
+
+    }
+
+    public void calcularComissao(){
+        if (totalDeVendas < 20000){
+            percentualDaComissao = 5;
+        } else {
+            percentualDaComissao = 10;
+        }
+
+        valorDaComissaoEmReais = totalDeVendas * percentualDaComissao / 100;
+
+        exibirResultados();
+
+    }
+
+    public void exibirResultados(){
+        System.out.println("-------------------------------------");
+        System.out.println("** R E S U L T A D O S **");
+        System.out.println("-------------------------------------");
+        System.out.println("VENDEDOR: " + nomeVendedor);
+        System.out.println("TOTAL DE VENDAS: R$" + totalDeVendas);
+        System.out.println("PERCENTUAL DA COMISSÃO: " + percentualDaComissao + "%");
+        System.out.println("VALOR DA COMISSÃO PAGA: R$ " + valorDaComissaoEmReais);
+        System.out.println("-------------------------------------");
+    }
+
+}
